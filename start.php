3bff9a66147ff2667592eaa54cafb5b58932600c
Steven Burger <46164156+cwsyth@users.noreply.github.com>
2022-12-15 14:32:08 +0100

    a) default routine

---
+++ start.php
@@ -0,0 +1,10 @@
+<?php
+    spl_autoload_register(function($class) {
+        include str_replace('\\', '/', $class) . '.php';
+    });
+
+    session_start();
+
+    define('CHAT_SERVER_URL', 'https://online-lectures-cs.thi.de/chat');
+    define('CHAT_SERVER_ID', 'f286aee2-128a-4e99-a839-4e07d87550eb'); # Ihre Collection ID
+?>
