b7b24987bc98dafd72abac09b587a61cd9a9d00f
Steven Burger <46164156+cwsyth@users.noreply.github.com>
2022-12-15 17:33:20 +0100

    backendservice till friendrequest

--- Utils/BackendService.php
+++ Utils/BackendService.php
@@ -2,6 +2,8 @@
 namespace utils;
 
 use utils\HttpClient;
+use model\User;
+use model\Friend;
 
 class BackendService {
     private $url;       // backend url
@@ -28,7 +30,7 @@ class BackendService {
             $_SESSION["chat_token"] = $result->token;
             return true;
         } catch(\Exception $e) {
-            echo "login error: " . $e . "<br>";
+            echo "<br>login error: " . $e . "<br>";
             return false;
         }
     }
@@ -40,10 +42,54 @@ class BackendService {
             $_SESSION['chat_token'] = $result->token;
             return true;
         } catch(\Exception $e) {
-            echo "register error: " . $e . "<br>";
+            echo "<br>register error: " . $e . "<br>";
+            return false;
+        }
+    }
+
+    public function loadUser($username) {
+        try {
+            $result = HttpClient::get($this->url . "/" . $this->id . "/user" . "/$username", $_SESSION["chat_token"]);
+            $user = User::fromJson($result); // method gets php object, $result already unpacked php object -> why is it called fromJson ???????🤡
+            return $user;
+        } catch(\Exception $e) {
+            echo "<br>load user error: " . $e . "<br>";
+            return null;
+        }
+    }   
+
+    public function saveUser($user) {
+        try {
+            $result = HttpClient::post($this->url . "/" . $this->id . "/user", $user, $_SESSION["chat_token"]);
+            return $result;
+        } catch (\Exception $e) {
+            echo "<br>save user error: " . $e . "<br>";
+            return false;
+        }
+    }
+
+    public function loadFriends() {
+        try {
+            $result = HttpClient::get($this->url . "/" . $this->id . "/friend", $_SESSION["chat_token"]);
+            $friends = array();
+            foreach($result as $value) {
+                $friends[] = Friend::fromJson($value);
+            }
+            return $friends;
+        } catch(\Exception $e) {
+            echo "<br>load friends error: " . $e . "<br>";
+            return false;
+        }
+    }
+
+    public function friendRequest($friend) {
+        try {
+            HttpClient::post($this->url . "/" . $this->id . "/friend", $friend ,$_SESSION["chat_token"]);
+            return true;
+        } catch(\Exception $e) {
+            echo "<br>load friends error: " . $e . "<br>";
             return false;
         }
     }
 }
-
 ?>
--- test.php
+++ test.php
@@ -1,22 +1,53 @@
 <?php
     require("start.php");
+    use model\Friend;
+    use model\User;
     
-    $user = new model\User("Test");
+    $user = new User("Test");
     $json = json_encode($user);
     echo $json . "<br>";
     $jsonObject = json_decode($json);
-    $newUser = model\User::fromJson($jsonObject);
+    $newUser = User::fromJson($jsonObject);
     var_dump($newUser);
+
+    echo "<br><br>";
 
     echo "TEST TEST";
     $service = new utils\BackendService(CHAT_SERVER_URL, CHAT_SERVER_ID);
     var_dump($service->test());
 
+    echo "<br><br>";
+
     echo "LOGIN TEST";
     var_dump($service->login("steven", "12345678"));
     var_dump($_SESSION);
 
+    echo "<br><br>";
+
     echo "REGISTER TEST";
     var_dump($service->register("steven", "12345678"));
     var_dump($_SESSION);
+
+    echo "<br><br>";
+
+    echo "LOAD USER TEST";
+    $user = $service->loadUser("steven");
+    var_dump($user);
+
+    echo "<br><br>";
+
+    echo "SAVE USER TEST";
+    var_dump($service->saveUser($user));
+    
+    echo "<br><br>";
+
+    echo "LOAD FRIENDS TEST";
+    $friends = $service->loadFriends();
+    var_dump($friends);
+
+    echo "<br><br>";
+
+    echo "FRIEND REQUEST TEST";
+    $friend = new Friend("Jerry");
+    var_dump($service->friendRequest($friend));
 ?>
